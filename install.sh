30926c6f849a780acce5e98ce2318e923ca597ab
hudson-newey <hudsonnewey@gmail.com>
2025-11-21 20:49:00 +1000

    Remove install.sh dev script

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-snippets_path="$HOME/.config/zed/snippets"
-
-mkdir -p "$snippets_path"
-cp -r ./snippets/* "$snippets_path/"
-
-echo "Zed Angular snippets installed successfully to $snippets_path"
